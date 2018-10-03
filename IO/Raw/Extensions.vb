6138e743b4d0e178a56dbcc0e3d781011ab2b57b
xieguigang <xie.guigang@gcmodeller.org>
2018-10-03 19:29:54 +0800

    how to read matrix?

---
+++ IO/Raw/Extensions.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Public Module Extensions
+
+    <Extension>
+    Public Iterator Function GetMatrix(raw As Reader, module$) As IEnumerable(Of DataSet)
+
+    End Function
+End Module
