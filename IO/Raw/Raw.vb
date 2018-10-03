a5d4d8db6eb43db0c7aa9ad115a44cf83a5c085d
xieguigang <xie.guigang@gcmodeller.org>
2018-10-03 15:11:34 +0800

    write headers

--- IO/Raw/Raw.vb
+++ IO/Raw/Raw.vb
@@ -1,7 +1,9 @@
 ﻿Imports System.IO
 Imports System.Reflection
+Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.IO
+Imports Microsoft.VisualBasic.Language
 Imports Microsoft.VisualBasic.Text
 Imports [Module] = Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps.DataFrameColumnAttribute
 
@@ -19,42 +21,42 @@ Public Class Raw : Implements IDisposable
     ''' </summary>
     ''' <returns></returns>
     <[Module]("Message-RNA")>
-    Public ReadOnly Property mRNAId As IReadOnlyCollection(Of String)
+    Public ReadOnly Property mRNAId As Index(Of String)
     ''' <summary>
     ''' 由基因转录出来的其他的RNA分子的编号列表
     ''' </summary>
     ''' <returns></returns>
     ''' 
     <[Module]("Component-RNA")>
-    Public ReadOnly Property RNAId As IReadOnlyCollection(Of String)
+    Public ReadOnly Property RNAId As Index(Of String)
     ''' <summary>
     ''' 由mRNA翻译出来的多肽链的Id列表
     ''' </summary>
     ''' <returns></returns>
     ''' 
     <[Module]("Polypeptide")>
-    Public ReadOnly Property Polypeptide As IReadOnlyCollection(Of String)
+    Public ReadOnly Property Polypeptide As Index(Of String)
     ''' <summary>
     ''' 由一条或者多条多肽链修饰之后得到的最终的蛋白质的编号列表
     ''' </summary>
     ''' <returns></returns>
     ''' 
     <[Module]("Protein")>
-    Public ReadOnly Property Proteins As IReadOnlyCollection(Of String)
+    Public ReadOnly Property Proteins As Index(Of String)
     ''' <summary>
     ''' 代谢物列表
     ''' </summary>
     ''' <returns></returns>
     ''' 
     <[Module]("Metabolite")>
-    Public ReadOnly Property Metabolites As IReadOnlyCollection(Of String)
+    Public ReadOnly Property Metabolites As Index(Of String)
     ''' <summary>
     ''' 反应过程编号列表
     ''' </summary>
     ''' <returns></returns>
     ''' 
     <[Module]("Reaction-Flux")>
-    Public ReadOnly Property Reactions As IReadOnlyCollection(Of String)
+    Public ReadOnly Property Reactions As Index(Of String)
 
 #End Region
 
@@ -111,16 +113,31 @@ Public Class Writer : Inherits Raw
         Dim modules As PropertyInfo() = GetType(Raw) _
             .GetProperties(PublicProperty) _
             .Where(Function(prop)
-                       Return prop.PropertyType Is GetType(IReadOnlyCollection(Of String))
+                       Return prop.PropertyType Is GetType(Index(Of String))
                    End Function) _
             .ToArray
 
         Call stream.Seek(0, SeekOrigin.Begin)
+        Call stream.Write(Raw.Magic, BinaryStringFormat.NoPrefixOrTermination)
 
         ' 二进制文件的结构为 
         ' - string 模块名称字符串，最开始为长度整形数
         ' - integer 有多少个编号
         ' - string ZERO 使用零结尾的编号字符串
+        For Each [mod] As PropertyInfo In modules
+            Dim modAttr = [mod].GetCustomAttribute(Of [Module])
+            Dim modAttrEmpty = modAttr Is Nothing OrElse modAttr.Name.StringEmpty
+            Dim name$ = modAttr.Name Or [mod].Name.When(modAttrEmpty)
+            Dim list$() = DirectCast([mod].GetValue(Me), Index(Of String)).Objects
 
+            Call stream.Write(name, BinaryStringFormat.DwordLengthPrefix)
+            Call stream.Write(list.Length)
+
+            For Each id As String In list
+                Call stream.Write(id, BinaryStringFormat.ZeroTerminated)
+            Next
+        Next
+
+        Return Me
     End Function
 End Class
