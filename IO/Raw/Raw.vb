66675bb033a4234b48567c8a9ddcd8918efaf08a
xieguigang <xie.guigang@gcmodeller.org>
2018-10-03 15:46:10 +0800

    write data in a cycle

--- IO/Raw/Raw.vb
+++ IO/Raw/Raw.vb
@@ -1,9 +1,11 @@
 ﻿Imports System.IO
 Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports Microsoft.VisualBasic.ComponentModel.Collection
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Data.IO
 Imports Microsoft.VisualBasic.Language
+Imports Microsoft.VisualBasic.Linq
 Imports Microsoft.VisualBasic.Text
 Imports [Module] = Microsoft.VisualBasic.ComponentModel.DataSourceModel.SchemaMaps.DataFrameColumnAttribute
 
@@ -100,6 +102,8 @@ End Class
 Public Class Writer : Inherits Raw
 
     ReadOnly stream As BinaryDataWriter
+    ReadOnly modules As New Dictionary(Of String, Index(Of String))
+    ReadOnly moduleIndex As New Index(Of String)
 
     Sub New(output As Stream)
         stream = New BinaryDataWriter(output, Encodings.UTF8WithoutBOM)
@@ -120,6 +124,9 @@ Public Class Writer : Inherits Raw
         Call stream.Seek(0, SeekOrigin.Begin)
         Call stream.Write(Raw.Magic, BinaryStringFormat.NoPrefixOrTermination)
 
+        Call Me.modules.Clear()
+        Call Me.moduleIndex.Clear()
+
         ' 二进制文件的结构为 
         '
         ' - string 模块名称字符串，最开始为长度整形数
@@ -138,7 +145,30 @@ Public Class Writer : Inherits Raw
             For Each id As String In list
                 Call stream.Write(id, BinaryStringFormat.ZeroTerminated)
             Next
+
+            Call Me.modules.Add(name, [module].GetValue(Me))
+            Call Me.moduleIndex.Add(name)
         Next
+
+        Return Me
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Write(module$, time#, cycle As Dictionary(Of String, Double)) As Writer
+        Return Write([module], time, cycle.Takes(modules([module]).Objects))
+    End Function
+
+    Public Function Write(module$, time#, data#()) As Writer
+
+        ' 一个循环之后得到的结果数据的写入的结构为
+        '
+        ' - double time 时间值
+        ' - byte 在header之中的module的索引号
+        ' - double() data块，每一个值的顺序是和header之中的id排布顺序是一样的，长度和header之中的id列表保持一致
+        '
+        Call stream.Write(time)
+        Call stream.Write(CByte(moduleIndex([module])))
+        Call stream.Write(data)
 
         Return Me
     End Function
