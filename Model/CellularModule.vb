4012aa3c62734d9aff5beb863d12c7152467e071
xieguigang <xie.guigang@gcmodeller.org>
2018-10-07 14:37:02 +0800

    cellular module data model

---
+++ Model/CellularModule.vb
@@ -0,0 +1,17 @@
+﻿Imports SMRUCC.genomics.Metagenomics
+
+''' <summary>
+''' A cellular system consist of two features:
+''' 
+''' + Genotype
+''' + Phenotype
+''' 
+''' </summary>
+Public Structure CellularModule
+
+    Public Taxonomy As Taxonomy
+
+    Public Genotype As Genotype
+    Public Phenotype As Phenotype
+
+End Structure
