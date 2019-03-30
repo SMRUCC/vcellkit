0ff7e709d4d751dc5e4e05371a3a0e84bb3c73e1
xieguigang <xie.guigang@gcmodeller.org>
2019-03-30 23:02:54 +0800

    regulation reaction channel model

--- Dynamics/Channel.vb
+++ Dynamics/Channel.vb
@@ -10,6 +10,112 @@ Public Class Channel
     Dim left As Variable()
     Dim right As Variable()
 
+    Public Property Forward As Regulation
+    Public Property Reverse As Regulation
+
+    Public ReadOnly Property Direction As Directions
+        Get
+            If Forward > Reverse Then
+                Return Directions.LeftToRight
+            ElseIf Reverse > Forward Then
+                Return Directions.RightToLeft
+            Else
+                Return Directions.Stop
+            End If
+        End Get
+    End Property
+
+    Public ReadOnly Property CoverLeft(Optional regulation As Double = 1) As Double
+        Get
+            Return minimalUnit(left, regulation)
+        End Get
+    End Property
+
+    Public ReadOnly Property CoverRight(Optional regulation As Double = 1) As Double
+        Get
+            Return minimalUnit(right, regulation)
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' 得到当前的物质内容所能够支撑的最小反应单位
+    ''' </summary>
+    ''' <param name="factors"></param>
+    ''' <param name="regulation"></param>
+    ''' <returns></returns>
+    Private Shared Function minimalUnit(factors As IEnumerable(Of Variable), regulation As Double) As Double
+        Return factors _
+            .Select(Function(v)
+                        Dim r = regulation * v.Coefficient
+
+                        If r > v.Mass.Value Then
+                            ' 消耗的已经超过了当前的容量
+                            ' 则最小的反应单位是当前的物质容量
+
+                            ' 如果某一个物质的容量是零，则表示没有反应物可以被利用了
+                            ' 则计算出来的最小反应单位是零
+                            ' 即此反应过程不可能会发生
+                            Return v.Mass.Value / v.Coefficient
+                        Else ' 能够正常的以当前的反应单位进行
+                            Return regulation
+                        End If
+                    End Function) _
+            .Min
+    End Function
+
+End Class
+
+Public Enum Directions
+    ''' <summary>
+    ''' 反应过程将不会发生
+    ''' </summary>
+    [Stop]
+    LeftToRight
+    RightToLeft
+End Enum
+
+''' <summary>
+''' 对反应过程的某一个方向的控制效应
+''' </summary>
+Public Class Regulation
+
+    Public Property Activation As Variable()
+    ''' <summary>
+    ''' 如果抑制的总量大于激活的总量，那么这个调控的反应过程将不会进行
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Inhibition As Variable()
+
+    Public ReadOnly Property Coefficient As Double
+        Get
+            Dim i = Inhibition.Sum(Function(v) v.Coefficient * v.Mass.Value)
+            Dim a = Activation.Sum(Function(v) v.Coefficient * v.Mass.Value)
+
+            If i >= a Then
+                ' 抑制的总量已经大于等于激活的总量的，则反应过程不会发生
+                Return 0
+            Else
+                Return a - i
+            End If
+        End Get
+    End Property
+
+    Public Shared Operator >(a As Regulation, b As Regulation) As Boolean
+        Return a.Coefficient > b.Coefficient
+    End Operator
+
+    Public Shared Operator <(a As Regulation, b As Regulation) As Boolean
+        Return a.Coefficient < b.Coefficient
+    End Operator
+
+    Public Shared Operator =(a As Regulation, b As Regulation) As Boolean
+        Return a.Coefficient = b.Coefficient
+    End Operator
+
+    Public Shared Operator <>(a As Regulation, b As Regulation) As Boolean
+        Return a.Coefficient <> b.Coefficient
+    End Operator
+
 End Class
 
 Public Class Variable
@@ -20,7 +126,7 @@ Public Class Variable
     ''' <returns></returns>
     Public ReadOnly Property Mass As Factor
     ''' <summary>
-    ''' 在反应过程之中的变异系数，每完成一个单位的反应过程，当前的<see cref="Mass"/>将会丢失这个系数相对应的数量的含量
+    ''' 在反应过程之中的变异系数，每完成一个单位的反应过程，当前的<see cref="Mass"/>将会丢失或者增加这个系数相对应的数量的含量
     ''' </summary>
     ''' <returns></returns>
     Public ReadOnly Property Coefficient As Double
