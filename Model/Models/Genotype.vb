1cff718e3a0293bdb4db0a0b606f0214ea3ffb0e
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-03-31 21:03:23 +0800

    add composition data

--- Model/Models/Genotype.vb
+++ Model/Models/Genotype.vb
@@ -59,9 +59,153 @@ Public Structure Genotype
     ''' </remarks>
     Dim centralDogmas As CentralDogma()
 
+    Dim RNAMatrix As RNAComposition()
+    Dim ProteinMatrix As ProteinComposition()
+
     Public Overrides Function ToString() As String
         Return $"{centralDogmas.Length} genes"
     End Function
 End Structure
 
+Public Class RNAComposition
 
+    Public Property geneID As String
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property A As Integer
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property U As Integer
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property G As Integer
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property C As Integer
+
+End Class
+
+Public Class ProteinComposition
+
+    Public Property proteinID As String
+
+    ''' <summary>
+    ''' L-Alanine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property A As Integer
+    ''' <summary>
+    ''' L-Arginine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property R As Integer
+    ''' <summary>
+    ''' L-Asparagine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property N As Integer
+    ''' <summary>
+    ''' L-Aspartic acid
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property D As Integer
+    ''' <summary>
+    ''' L-Cysteine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property C As Integer
+    ''' <summary>
+    ''' L-Glutamic acid
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property E As Integer
+    ''' <summary>
+    ''' L-Glutamine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Q As Integer
+    ''' <summary>
+    ''' Glycine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property G As Integer
+    ''' <summary>
+    ''' L-Histidine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property H As Integer
+    ''' <summary>
+    ''' L-Isoleucine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property I As Integer
+    ''' <summary>
+    ''' L-Leucine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property L As Integer
+    ''' <summary>
+    ''' L-Lysine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property K As Integer
+    ''' <summary>
+    ''' L-Methionine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property M As Integer
+    ''' <summary>
+    ''' L-Phenylalanine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property F As Integer
+    ''' <summary>
+    ''' L-Proline
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property P As Integer
+    ''' <summary>
+    ''' L-Serine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property S As Integer
+    ''' <summary>
+    ''' L-Threonine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property T As Integer
+    ''' <summary>
+    ''' L-Tryptophan
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property W As Integer
+    ''' <summary>
+    ''' L-Tyrosine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Y As Integer
+    ''' <summary>
+    ''' L-Valine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property V As Integer
+    ''' <summary>
+    ''' L-Selenocysteine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property U As Integer
+    ''' <summary>
+    ''' L-Pyrrolysine
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property O As Integer
+
+End Class
