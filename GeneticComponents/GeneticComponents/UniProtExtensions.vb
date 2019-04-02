56c10dd81263ebb5cb98de0b67621cddfe27652e
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-04-02 19:09:22 +0800

    add test reader

--- GeneticComponents/GeneticComponents/UniProtExtensions.vb
+++ GeneticComponents/GeneticComponents/UniProtExtensions.vb
@@ -1,11 +1,21 @@
-﻿Imports SMRUCC.genomics.Assembly.Uniprot.XML
+﻿Imports System.Runtime.CompilerServices
+Imports SMRUCC.genomics.Assembly.Uniprot.XML
 
 ''' <summary>
 ''' Create genetic components from Uniprot database.
 ''' </summary>
 Public Module UniProtExtensions
 
+    <Extension>
     Public Iterator Function CreateDump(uniprot As IEnumerable(Of entry)) As IEnumerable(Of GeneticNode)
+        For Each protein As entry In uniprot.Where(Function(g) Not g.sequence Is Nothing)
+            Dim KO As dbReference = protein.KO
 
+            If KO Is Nothing Then
+                Continue For
+            End If
+
+
+        Next
     End Function
 End Module
