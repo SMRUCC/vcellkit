6a4ce54f94430e53f8112577eed6afe4c9b44cfc
关倩倩 <amethyst.asuka@gcmodeller.org>
2019-04-02 18:46:58 +0800

    add extension for uniprot create genetic components node

---
+++ GeneticComponents/GeneticComponents/UniProtExtensions.vb
@@ -0,0 +1,11 @@
+﻿Imports SMRUCC.genomics.Assembly.Uniprot.XML
+
+''' <summary>
+''' Create genetic components from Uniprot database.
+''' </summary>
+Public Module UniProtExtensions
+
+    Public Iterator Function CreateDump(uniprot As IEnumerable(Of entry)) As IEnumerable(Of GeneticNode)
+
+    End Function
+End Module
