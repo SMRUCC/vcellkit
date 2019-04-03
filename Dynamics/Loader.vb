0a4a2c6453ccc2a32dddcfef1edc4116de3880b4
xieguigang <xie.guigang@gcmodeller.org>
2019-04-03 22:50:04 +0800

    mass table

--- Dynamics/Loader.vb
+++ Dynamics/Loader.vb
@@ -2,13 +2,20 @@
 Imports Microsoft.VisualBasic.ComponentModel.TagData
 Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel.Repository
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics
 
 ''' <summary>
 ''' Module loader
 ''' </summary>
-Public Module Loader
+Public Class Loader
 
-    <Extension>
+    Dim define As Definition
+
+    Sub New(define As Definition)
+        Me.define = define
+    End Sub
+
     Public Function CreateEnvironment(cell As CellularModule) As Vessel
         Dim channels As New List(Of Channel)
         Dim massTable As New Dictionary(Of String, Factor)
@@ -69,7 +76,7 @@ Public Module Loader
     End Function
 
     <Extension>
-    Private Function transcriptionTemplate(massTable As Dictionary(Of String, Factor), geneID$, matrix As Dictionary(Of String, RNAComposition)) As Variable()
+    Private Function transcriptionTemplate(massTable As Dictionary(Of String, Factor), geneID$, matrix As Dictionary(Of String, RNAComposition), define As Definition) As Variable()
         Return matrix(geneID) _
             .Where(Function(i) i.Value > 0) _
             .Select(Function(base) massTable.variable(base.Name, base.Value)) _
@@ -83,49 +90,74 @@ Public Module Loader
             .Select(Function(aa) massTable.variable(aa.Name, aa.Value)) _
             .AsList + massTable.template(mRNA)
     End Function
+End Class
 
-    <Extension>
-    Private Function variables(compounds As IEnumerable(Of String), massTable As Dictionary(Of String, Factor), factor As Double) As IEnumerable(Of Variable)
-        Return compounds.Select(Function(cpd) massTable.variable(cpd, factor))
+Public Class MassTable : Implements IRepository(Of String, Factor)
+
+    Dim massTable As Dictionary(Of String, Factor)
+
+    Public Sub Delete(key As String) Implements IRepositoryWrite(Of String, Factor).Delete
+        If massTable.ContainsKey(key) Then
+            Call massTable.Remove(key)
+        End If
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Sub AddOrUpdate(entity As Factor, key As String) Implements IRepositoryWrite(Of String, Factor).AddOrUpdate
+        massTable(key) = entity
+    End Sub
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function variables(compounds As IEnumerable(Of String), factor As Double) As IEnumerable(Of Variable)
+        Return compounds.Select(Function(cpd) Me.variable(cpd, factor))
     End Function
 
-    <Extension>
-    Private Function variables(compounds As IEnumerable(Of FactorString(Of Double)), massTable As Dictionary(Of String, Factor)) As IEnumerable(Of Variable)
-        Return compounds.Select(Function(cpd) massTable.variable(cpd.text, cpd.factor))
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function variables(compounds As IEnumerable(Of FactorString(Of Double))) As IEnumerable(Of Variable)
+        Return compounds.Select(Function(cpd) Me.variable(cpd.text, cpd.factor))
     End Function
 
-    <Extension>
-    Private Iterator Function variables(massTable As Dictionary(Of String, Factor), complex As Protein) As IEnumerable(Of Variable)
+    Public Iterator Function variables(complex As Protein) As IEnumerable(Of Variable)
         For Each compound In complex.compounds
-            Yield massTable.variable(compound)
+            Yield Me.variable(compound)
         Next
         For Each peptide In complex.polypeptides
-            Yield massTable.variable(peptide)
+            Yield Me.variable(peptide)
         Next
     End Function
 
-    <Extension>
-    Private Function variable(massTable As Dictionary(Of String, Factor), mass As String, Optional coefficient As Double = 1) As Variable
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function variable(mass As String, Optional coefficient As Double = 1) As Variable
         Return New Variable(massTable(mass), coefficient, False)
     End Function
 
-    <Extension>
-    Private Function template(massTable As Dictionary(Of String, Factor), mass As String) As Variable
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function template(mass As String) As Variable
         Return New Variable(massTable(mass), 1, True)
     End Function
 
-    ''' <summary>
-    ''' 重置反应环境模拟器之中的内容
-    ''' </summary>
-    ''' <param name="envir"></param>
-    ''' <param name="massInit"></param>
-    ''' <returns></returns>
-    <Extension>
-    Public Function Reset(envir As Vessel, massInit As Dictionary(Of String, Double)) As Vessel
-        For Each mass As Factor In envir.Mass
-            mass.Value = massInit(mass.ID)
-        Next
-
-        Return envir
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function Exists(key As String) As Boolean Implements IRepositoryRead(Of String, Factor).Exists
+        Return massTable.ContainsKey(key)
     End Function
-End Module
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetByKey(key As String) As Factor Implements IRepositoryRead(Of String, Factor).GetByKey
+        Return massTable.TryGetValue(key)
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetWhere(clause As Func(Of Factor, Boolean)) As IReadOnlyDictionary(Of String, Factor) Implements IRepositoryRead(Of String, Factor).GetWhere
+        Return massTable.Values.Where(clause).ToDictionary
+    End Function
+
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
+    Public Function GetAll() As IReadOnlyDictionary(Of String, Factor) Implements IRepositoryRead(Of String, Factor).GetAll
+        Return massTable
+    End Function
+
+    Public Function AddNew(entity As Factor) As String Implements IRepositoryWrite(Of String, Factor).AddNew
+        massTable(entity.ID) = entity
+        Return entity.ID
+    End Function
+End Class
