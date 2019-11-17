76e82906cc0940add8f9c884f60aa4496c82f1cc
xieguigang <xie.guigang@gcmodeller.org>
2019-11-17 12:42:52 +0800

    save mass contents

--- Dynamics/Engine/OmicsDataAdapter.vb
+++ Dynamics/Engine/OmicsDataAdapter.vb
@@ -1,4 +1,5 @@
-﻿Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
+﻿Imports Microsoft.VisualBasic.Linq
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Model
 
 Namespace Engine
 
@@ -24,8 +25,10 @@ Namespace Engine
         Dim saveFlux As OmicsTuple(Of DataStorageDriver)
 
         Sub New(model As CellularModule, mass As OmicsTuple(Of DataStorageDriver), flux As OmicsTuple(Of DataStorageDriver))
-            saveMass = mass
-            saveFlux = flux
+            Me.saveMass = mass
+            Me.saveFlux = flux
+            Me.mass = GetMassTuples(model)
+            Me.flux = GetFluxTuples(model)
         End Sub
 
         Public Sub MassSnapshot(iteration As Integer, data As Dictionary(Of String, Double))
@@ -39,5 +42,29 @@ Namespace Engine
             Call saveFlux.proteome(iteration, data.Subset(flux.proteome))
             Call saveFlux.metabolome(iteration, data.Subset(flux.metabolome))
         End Sub
+
+        Public Shared Function GetMassTuples(model As CellularModule) As OmicsTuple(Of String())
+            Dim RNA = model.Genotype.centralDogmas _
+                .Select(Function(gene) gene.RNA.Name) _
+                .ToArray
+            Dim protein = model.Genotype.centralDogmas _
+                .Where(Function(gene) Not gene.IsRNAGene) _
+                .Select(Function(gene) gene.polypeptide) _
+                .ToArray
+            Dim metabolites = model.Phenotype.fluxes _
+                .Select(Function(flux)
+                            Return flux.products.AsList + flux.substrates
+                        End Function) _
+                .IteratesALL _
+                .Select(Function(mass) mass.text) _
+                .Distinct _
+                .ToArray
+
+            Return New OmicsTuple(Of String())(RNA, protein, metabolites)
+        End Function
+
+        Public Shared Function GetFluxTuples(model As CellularModule) As OmicsTuple(Of String())
+
+        End Function
     End Class
 End Namespace
