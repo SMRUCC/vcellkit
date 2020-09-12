f3a24f74a38e546d7b3ea667ba6218d9b173e640
xieguigang <xie.guigang@gcmodeller.org>
2020-09-12 18:43:13 +0800

    assembly command

---
+++ Compiler/AssemblyScript/Commands/From.vb
@@ -0,0 +1,12 @@
+﻿Namespace AssemblyScript.Commands
+
+    ''' <summary>
+    ''' from a base model
+    ''' </summary>
+    Public Class From : Inherits Command
+
+        Public Overrides Function Execute(env As Environment) As Object
+            Throw New NotImplementedException()
+        End Function
+    End Class
+End Namespace
