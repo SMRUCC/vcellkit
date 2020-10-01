c743318431b933d2ac825a49188655619a2e77fe
xieguigang <xie.guigang@gcmodeller.org>
2020-10-01 19:12:28 +0800

    parse modification data

--- Compiler/AssemblyScript/Commands/Add.vb
+++ Compiler/AssemblyScript/Commands/Add.vb
@@ -1,9 +1,15 @@
-﻿Namespace AssemblyScript.Commands
+﻿Imports SMRUCC.genomics.GCModeller.Compiler.AssemblyScript.Script
+
+Namespace AssemblyScript.Commands
 
     ''' <summary>
     ''' add item nodes to cellular network
     ''' </summary>
     Public Class Add : Inherits Modification
+
+        Sub New(tokens As Token())
+
+        End Sub
 
         Public Overrides Function Execute(env As Environment) As Object
             Throw New NotImplementedException()
--- Compiler/AssemblyScript/Commands/Delete.vb
+++ Compiler/AssemblyScript/Commands/Delete.vb
@@ -1,9 +1,15 @@
-﻿Namespace AssemblyScript.Commands
+﻿Imports SMRUCC.genomics.GCModeller.Compiler.AssemblyScript.Script
+
+Namespace AssemblyScript.Commands
 
     ''' <summary>
     ''' delete node items from cellular network
     ''' </summary>
     Public Class Delete : Inherits Modification
+
+        Sub New(tokens As Token())
+
+        End Sub
 
         Public Overrides Function Execute(env As Environment) As Object
             Throw New NotImplementedException()
