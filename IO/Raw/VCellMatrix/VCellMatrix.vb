82a9709625631e6eece3d70dc507ae34b92ca670
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2025-09-09 22:02:08 +0800

    add vcell mattrix model

---
+++ IO/Raw/VCellMatrix/VCellMatrix.vb
@@ -0,0 +1,21 @@
+﻿Imports SMRUCC.genomics.Analysis.HTS.DataFrame
+Imports SMRUCC.genomics.GCModeller.ModellingEngine.Dynamics.Core
+
+Public Class VCellMatrix
+
+    ''' <summary>
+    ''' the molecule expression data
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property expressionData As DataFrameRow()
+    ''' <summary>
+    ''' the biological process activity expression data
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property fluxomics As DataFrameRow()
+
+    Public Property cellularGraph As FluxEdge()
+    Public Property modules As Dictionary(Of String, String())
+    Public Property compartmentIds As String()
+
+End Class
